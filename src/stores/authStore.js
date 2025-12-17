20a9496d56d0db5b9fce4d3c11ccc856543bd507
mohan manirul <fiforeg@gmail.com>
2025-12-18 00:58:04 +0600

    success message and error message show ok

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import apiClient from './../services/axiosClient';
 import { useRouter } from "vue-router";
+import cogoToast from "cogo-toast";
 
 export const useAuthStore =  defineStore('auth',()=>{
     //state
@@ -27,9 +28,30 @@ export const useAuthStore =  defineStore('auth',()=>{
         const res = await apiClient.post("/login", credentials);
         token.value = res.data.data.token ;
         localStorage.setItem("token", token.value);
+        cogoToast.success('Login Success',{position:'top-right'})
         return true;
       } catch (error) {
         console.log(error);
+        
+        if(error.response?.data?.errors){
+            const errors = error.response.data.errors;
+            for(const field in errors ){
+                errors[field].forEach((msg)=>{
+                  cogoToast.error(msg, { position: "top-right" });
+                });
+            }
+        }
+        // invalid credentials
+        else if (error.response?.data?.message) {
+          cogoToast.error(error.response.data.message, {
+            position: "top-right"
+           });
+        }
+        else{
+          cogoToast.error('Something went wrong', {
+            position: "top-right"
+          });
+        }
       }
     };
 
